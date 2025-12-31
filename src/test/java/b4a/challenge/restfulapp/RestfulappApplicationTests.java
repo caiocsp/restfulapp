ff427d311bb6c8af20942234c13bbc1bbfb74b9a
caiocsp <caio.c.paulino@gmail.com>
2025-12-31 12:22:03 -0300

    Fix: Context load tests

--- src/test/java/b4a/challenge/restfulapp/RestfulappApplicationTests.java
+++ src/test/java/b4a/challenge/restfulapp/RestfulappApplicationTests.java
@@ -2,8 +2,12 @@ package b4a.challenge.restfulapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import caiocsp.project.restfulapp.RestfulappApplication;
 
 @SpringBootTest
+@ContextConfiguration(classes = RestfulappApplication.class)
 class RestfulappApplicationTests {
 
 	@Test
